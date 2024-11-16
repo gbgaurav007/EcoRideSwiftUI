88ed68612b921292128fe48020f033f9acab7d82
Gaurav Bansal <gbgauravbansal007@gmail.com>
2024-11-17 02:29:49 +0530

    Create EcoRide

---
+++ EcoRide/EcoRide/EcoRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoRideApp.swift
+//  EcoRide
+//
+//  Created by Gaurav Bansal on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoRide/EcoRideTests/EcoRideTests.swift
@@ -0,0 +1,16 @@
+//
+//  EcoRideTests.swift
+//  EcoRideTests
+//
+//  Created by Gaurav Bansal on 17/11/24.
+//
+
+import Testing
+
+struct EcoRideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
